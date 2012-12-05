637060f6a9b7119594033afdbe9925a216d21b74
Daniel Nishiguchi <lezgosurfin@gmail.com>
2012-12-04 22:04:02 -0800

    BLAH

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121205024417) do
+ActiveRecord::Schema.define(:version => 20121205060036) do
 
-  create_table "Statuses", :force => true do |t|
+  create_table "statuses", :force => true do |t|
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
   end
 
-  add_index "Statuses", ["user_id"], :name => "index_statuses_on_user_id"
+  add_index "statuses", ["user_id"], :name => "index_statuses_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
